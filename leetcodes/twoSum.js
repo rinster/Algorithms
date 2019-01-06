58824aa4e4d23b1f5bee7d34170f3df57e96c2f1
Erine Natnat <enatnat.08@gmail.com>
2019-01-06 15:06:55 -0800

    Initial commit

---
+++ leetcodes/twoSum.js
@@ -0,0 +1,30 @@
+//**************************************************************
+//Given an array of integers, return indices of the two numbers 
+//such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, 
+//and you may not use the same element twice.
+//**************************************************************
+
+//Example:
+//Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+// Solution--------------------------
+var twoSum = function(nums, target) {
+    //initialize object that will store the nums
+    const ints = {}; 
+    
+    //loop through array
+    for (let i=0; i<nums.length; i++) { 
+
+        //if ints at a specific index of an array is greater than zero enter check pass
+        if (ints[nums[i] ] >=0) {
+
+            //store that nums in an array
+            return [ints[nums[i]], i];
+        }
+        //then, get the second index/nums by minusing the target and the stored num to get the index
+        ints[target-nums[i]]= i
+    }
+};
---
+++ uDemy_algos/charCount.js
@@ -0,0 +1,41 @@
+//**************************************************************
+//Write a function that takes in a string and returns 
+//counts of each character in the string. 
+//**************************************************************
+
+//Examples:
+
+// charCount("aaaa")
+// {
+//     a:4
+// }
+
+//charCount("hello")
+// {
+//     h:1,
+//     e:1,
+//     l:2,
+//     o:1
+// }
+
+// Solution--------------------------
+function charCount(str) {
+    //make object to return at end
+    var result = {};
+    //loop over string for each character
+    for(var i=0; i<str.length; i++) {
+        //store str[i]--lowercased--into a variable 
+        var char = str[i].toLowerCase();
+        if(result[char] > 0) {
+            //if char is a num/letter AND key in object, add one to count
+            result[char]++;
+        } else {
+            //if char is a num/letter AND  not in object, add it and set value to 1
+            result[char] = 1
+        }
+    }
+    //return object at end
+    return result;
+        //if char is something else(space, period, etc), do nothing
+}
+
