66a9c937a2df51888c55aadac1a256459159bfea
Erine Natnat <enatnat.08@gmail.com>
2019-01-06 15:11:13 -0800

    Added sum of two nums and calculate century from year

--- CodeSignal/sumOfTwoNums.js
+++ CodeSignal/sumOfTwoNums.js
@@ -1,1 +1,8 @@
+//**************************************************************
 // Write a function that returns the sum of two numbers.
+//**************************************************************
+
+function add(param1, param2) {
+    let sum = param1 + param2
+    return sum
+}
